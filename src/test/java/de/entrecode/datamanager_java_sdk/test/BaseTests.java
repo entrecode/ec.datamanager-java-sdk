1f5fd4b33dd94ab41c165b64fe90334ef8d58ab1
Simon Scherzinger <scherzinger@entrecode.de>
2015-05-25 15:16:57 +0200

    Gradle Junit test logging added. Basic test for tests added.

---
+++ src/test/java/de/entrecode/datamanager_java_sdk/test/BaseTests.java
@@ -0,0 +1,16 @@
+package de.entrecode.datamanager_java_sdk.test;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Created by simon on 25.05.15.
+ */
+public class BaseTests {
+    @Test
+    public void testThatTestsWork() {
+        int a = 1, b = 2;
+        int c = 3;
+        assertEquals(a + b, c);
+    }
+}
